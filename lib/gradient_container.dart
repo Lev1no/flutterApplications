d6e62e67be7d3517f4e9b4e0bd8b8242b8ed6c22
Leevi Alanen <leevi.alanen1@gmail.com>
2024-05-29 16:10:11 +0300

    finalizing functionality of the dice

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:dice_app/dice_roller.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
 
   final Color color1;
   final Color color2;
@@ -20,27 +25,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-images/dice-2.png',
-              width: 200,
-            ),
-            TextButton(
-              onPressed: () {},
-              style: TextButton.styleFrom(
-                  padding: const EdgeInsets.only(
-                    top: 20,
-                  ),
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle(
-                    fontSize: 28,
-                  )),
-              child: const Text('Roll Dice'),
-            )
-          ],
-        ),
+        child: DiceRoller(),
       ),
     );
   }
